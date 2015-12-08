519dff8b88a9460ada3b015e0ea68e67a2585921
wbadart <wbadart@live.com>
2015-12-07 23:36:06 -0500

    Added negative number handling to eval

--- code/head.h
+++ code/head.h
@@ -3,8 +3,18 @@
 #include <string.h>
 #include <ctype.h>
 
+typedef struct Expression_s{
+    double a;
+    double b;
+    char op;
+} Expression;
+
+extern char *valids;
+
 void parse(char *);
 int lastIndexOf(char *, char);
 int firstIndexOf(char *, char, int);
 double eval(char str[200]);
 double str2dbl(char *);
+int isexpr(char *, int, int);
+int isvalidchar(char);
--- code/lib.c
+++ code/lib.c
@@ -1,5 +1,7 @@
 #include "head.h"
 
+char *valids = "+-*/^.";
+
 void parse(char *str){
     int n, i, len = strlen(str);
     //order of operations-> compute () first
@@ -27,7 +29,7 @@ double eval(char str[200]){
     int j = 0, i = 0;
     char operand_a[100], operand_b[100], op;
     double a, b, result;
-    while(isdigit(str[i]) || str[i] == '.'){
+    while((isdigit(str[i]) || str[i] == '.' || str[i] == '-') && i < 200){
         operand_a[i] = str[i];
         i++;
     }
@@ -35,7 +37,7 @@ double eval(char str[200]){
     a = str2dbl(operand_a);
     op = str[i];
     i++;
-    while(isdigit(str[i]) || str[i] == '.'){
+    while((isdigit(str[i]) || str[i] == '.' || str[i] == '-') && i < 200){
         operand_b[j] = str[i];
         i++; j++;
     }
@@ -62,17 +64,20 @@ double eval(char str[200]){
 }
 
 double str2dbl(char *str){
-    int j = 0, i, n = firstIndexOf(str, '.', 0);
+    int j = 0, i, n = firstIndexOf(str, '.', 0), neg = 0;
     double result = 0;
+    if(str[0] == '-'){
+        neg = 1; j = 1;
+    }
     if(n == -1){
         n = strlen(str);
-        for(i = n - 1; i >= 0; i--){
+        for(i = n - 1 - neg; i >= 0; i--){
             result += pow(10, i) * (str[j] - 48);
             j++;
         }
     }else{
-        j = n - 1;
-        for(i = 0; i < n; i++){
+        j = n - 1 - neg;
+        for(i = neg?1:0; i < n; i++){
             result += pow(10, j) * (str[i] - 48);
             j--;
         }
@@ -82,7 +87,32 @@ double str2dbl(char *str){
             j--;
         }
     }
+    if(neg) result *= -1;
     return result;
+}
+
+int isexpr(char *str, int start, int end){
+    int i = start, result = 1, decs = 0;
+    while((isdigit(str[i]) || str[i] == '.') && i < end){
+        if(str[i] == '.') decs++;
+        if(!isdigit(str[i + 1] && !isvalidchar(str[i + 1]))) result = 0;
+        i++;
+    }
+    i++;
+    while((isdigit(str[i]) || str[i] == '.') && i < end){
+        if(str[i] == '.') decs++;
+        if(!isdigit(str[i + 1] && !isvalidchar(str[i + 1]))) result = 0;
+        i++;
+    }
+    if(decs > 2) result = 0;
+    return result;
+}
+
+int isvalidchar(char c){
+    int i, match = 0;
+    for(i = 0; i < strlen(valids); i++)
+        if(c == valids[i]) match = 1;
+    return match;
 }
 
 int lastIndexOf(char *str, char target){
