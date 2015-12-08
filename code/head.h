de970d5eeb664a5eb6e2ce530c0d8ad166a6daf2
wbadart <wbadart@live.com>
2015-12-07 20:48:32 -0500

    Added functions Two main funcitons will be parse and eval. Parse determines which functions to use and eval returns the (double)result of a (char *)expression.

--- code/head.h
+++ code/head.h
@@ -5,3 +5,6 @@
 
 void parse(char *);
 int lastIndexOf(char *, char);
+int firstIndexOf(char *, char, int);
+double eval(char *);
+double str2dbl(char *);
--- code/lib.c
+++ code/lib.c
@@ -1,18 +1,53 @@
 #include "head.h"
 
-void parse(char *usrin){
-    int n, i, len = strlen(usrin);
+void parse(char *str){
+    int n, i, len = strlen(str);
     //order of operations-> compute () first
-    n = lastIndexOf(usrin, '(');
-    if(n != -1){
-        switch(usrin[n - 1]){
+    n = lastIndexOf(str, '(');
+        switch(str[n - 1]){
             case 's':
                 //cos
+                break;
             case 'n':
                 //sin tan or ln
+                break;
             case 'g':
                 //log
-            default
+                break;
+            case '^':
+                //exp
+                break;
+            default:
+                //arithmatic
+                break;
+    }
+}
+
+double eval(char *str){
+    int j = 0, i = 0;
+    char operand_a[100], operand_b[100], operator;
+    while(isdigit(str[i]) || str[i] != '.'){
+        operand_a[i] = str[i] - 48;
+        i++;
+    }
+    operand_a[i] = '\0';
+    operator = str[i];
+    i++;
+    while(isdigit(str[i]) || str[i] != '.'){
+        operand_b[j] = str[i];
+        i++; j++;
+    }
+    operand_b[j] = '\0';    
+}
+
+double str2dbl(char *str){
+    int j = 0, i, n, dec = firstIndexOf(str, '.', 0);
+    double result = 0;
+    if(dec = -1){
+        n = strlen(str);
+        for(i = n - 1; i >= 0; i--){
+            result += pow(10, i) * (str[j] - 48);
+            j++;
         }
     }
 }
@@ -20,6 +55,12 @@ void parse(char *usrin){
 int lastIndexOf(char *str, char target){
     int i = strlen(str);
     for(i = i - 1; i >= 0; i--)
+        if(str[i] == target) return i;
+    return -1;
+}
+int firstIndexOf(char * str, char target, int max){
+    int i;
+    for(i = max; i < strlen(str); i++)
         if(str[i] == target) return i;
     return -1;
 }
