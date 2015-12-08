56fb4aeeb3756774bb8856610fd626bd30e0188c
Catherine Elizabeth Badart <cbadart@darrow3.cc.nd.edu>
2015-12-07 20:39:49 -0500

    added stuff to main: prompts user for command, stores in str, makes sure it isnt an exit case and then calls parse.

--- code/main.c
+++ code/main.c
@@ -1,6 +1,24 @@
 #include "head.h"
 
 int main(void){
-    test();
+    printf("----SCIENTIFIC CALCULATOR----\n\n");
+    printf("Enter QUIT to stop\n");
+    char str[20];
+    double answer;
+    while(1)
+    {
+    	printf("Enter a command: ");
+    	scanf("%s", &str);
+    	if(strcmp(str, "QUIT") == 0)
+    		break;
+    	else if(strcmp(str, "Quit") == 0)
+    		break;
+    	else if(strcmp(str, "quit") == 0)
+    		break;
+    	//else
+    	//	parse(str);
+       	printf("%lf\n", answer);
+    }
+    //test();
     return 1;
 }
